90bcf87d37e432f9e0a809141435db06e81fc8d7
Yen-Ting Li <ytli.tw@gmail.com>
2024-12-05 12:39:28 +0800

    fix: csv parsing bug

--- src/hotels/mappers/dto.mapper.ts
+++ src/hotels/mappers/dto.mapper.ts
@@ -1,11 +1,23 @@
 import { plainToInstance } from 'class-transformer';
-import { validateOrReject } from 'class-validator';
+import { validateOrReject, ValidationError } from 'class-validator';
 import { CreateHotelDto } from '../dtos/requests/create-hotel.dto';
 
 export async function mapRecordToCreateHotelDto(
-  record: Record<string, string>,
+  record: Record<string, any>,
 ): Promise<CreateHotelDto> {
   try {
+    if (record.isOpen !== '1' && record.isOpen !== '0') {
+      const error = new ValidationError();
+      error.property = 'isOpen';
+      error.constraints = {
+        isIn: 'isOpen must be either 1 or 0',
+      };
+      error.value = record.isOpen;
+      throw [error];
+    } else {
+      record.isOpen = record.isOpen === '1';
+    }
+
     const createDto = plainToInstance(CreateHotelDto, record, {
       enableImplicitConversion: true,
     });
