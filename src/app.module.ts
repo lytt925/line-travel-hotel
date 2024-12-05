2fd843ca738aaa6043a045a9ec5f6c0dd71880bc
Yen-Ting Li <ytli.tw@gmail.com>
2024-12-05 12:00:27 +0800

    ci wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HotelsModule } from './hotels/hotels.module';
-import { ConfigModule } from './common/configs/config.module';
+import { ConfigModule } from '@nestjs/config';
 import { HttpLoggerMiddleware } from './common/middlewares/logger.middleware';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 @Module({
-  imports: [ConfigModule, DatabaseModule, HotelsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: ['production', 'test'].includes(process.env.NODE_ENV), // use shell env vars
+      envFilePath: '.env.development',
+    }),
+    DatabaseModule,
+    HotelsModule,
+  ],
   providers: [
     {
       provide: APP_INTERCEPTOR,
